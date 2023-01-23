bc161018d5755c966f110daa019b123032f06939
7nmw <7intur@gmail.com>
2023-01-23 17:22:52 +0300

    move to sports.ru

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-#from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,9 +5,8 @@ from time import sleep
 
 driver = webdriver.Chrome(r"C:\Users\dinislam\PycharmProjects\pythonProject1\Test-2\chromedriver.exe")
 
-driver.get("https://google.com")
-driver.find_element(By.XPATH, "//input[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN)
-
+driver.get("https://www.sports.ru/tribuna/blogs/england/3110612.html#supertop")
+(driver.find_elements(By.XPATH, "//*[@id=\"branding-layout\"]/div/div/div/main/div/article/div/p/img"))[9].click()
 sleep(2)
-driver.save_screenshot('sf.png')
+driver.save_screenshot('sport.png')
 driver.quit()
